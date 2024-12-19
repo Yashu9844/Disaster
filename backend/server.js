af6ae6cc23e72aa8bd8a09f106487b5578daee72
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-12-19 14:08:13 +0530

    clean conflicts

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
-const express = require("express");
-const dotenv = require("dotenv");
-const { default: mongoose } = require("mongoose");
-=======
+
+
 import 'dotenv/config' // To read CLERK_SECRET_KEY and CLERK_PUBLISHABLE_KEY
 import express from 'express'
 import { ClerkExpressRequireAuth, ClerkExpressWithAuth } from '@clerk/clerk-sdk-node'
 import cors from 'cors'
->>>>>>> a8a96c6de90de7d2eeec78b08e8e4249af38cf86
+import mongoose from 'mongoose'
 
 const port = process.env.PORT || 3000
 
@@ -20,19 +17,18 @@ app.get('/protected-auth-required', ClerkExpressRequireAuth(), (req, res) => {
   res.json(req.auth)
 })
 
-<<<<<<< HEAD
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
     console.log("DB connected")
 }).catch(err => console.log(err))
-=======
+
 // Use the lax middleware that returns an empty auth object when unauthenticated
 app.get('/protected-auth-optional', ClerkExpressWithAuth(), (req, res) => {
   console.log(req.auth)
 
   res.json(req.auth)
 })
->>>>>>> a8a96c6de90de7d2eeec78b08e8e4249af38cf86
+
 
 // Error handling middleware function
 app.use((err, req, res, next) => {
